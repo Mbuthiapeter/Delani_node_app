5d25f6e32142f7584be0ee923ce13f1066718334
Mbuthia Peter <mbuthiapetermbuthia@gmail.com>
2023-07-17 10:17:28 +0300

    Added the server file

---
+++ server.js
@@ -0,0 +1,14 @@
+let http = require('http');
+let start = function(){
+    function onRequest(request, response){
+        console.log("Request received!")
+        response.writeHead(200, {"Content-type": "text/plain"});
+        response.write("Hello World");
+        response.end();
+    }
+    http.createServer(onRequest).listen(8000);
+    console.log("Server has started.")
+}
+
+
+   
