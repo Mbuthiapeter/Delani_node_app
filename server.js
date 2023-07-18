25c4db5578bf2229045bb945f057565658123f75
Mbuthia Peter <mbuthiapetermbuthia@gmail.com>
2023-07-18 13:16:16 +0300

    Added loging in a loger

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 let http = require('http');
 let url = require('url');
+let Logger = require('./logger');
+let logger = new Logger('Server');
 
 let start = function(route,handle){
     function onRequest(request, response){
         let pathname = url.parse(request.url).pathname
         console.log("Request for " + pathname + " has been received.") 
+        logger.info("Request for " + pathname + " has been received with the request method " + request.method)
         
         route(handle, pathname,response);
-
-        response.writeHead(200, {"Content-type": "text/plain"});
-        response.write("Hello World");
-        response.end();
     }
-    http.createServer(onRequest).listen(8000);
-    console.log("Server has started.")
+    
+   let PORT = process.env.PORT || 8000;
+
+   http.createServer(onRequest).listen(PORT);
+   logger.info(`Server has started on Port: ${PORT}`)
+   console.log(`Server has started on Port: ${PORT}`);
 
 }
 exports.start = start;
